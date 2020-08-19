2c11cfbc8c085f0497d2a0d78ee5997778f25b3c
dido1998 <adidolkar123@gmail.com>
2020-08-19 17:04:09 +0530

    chem data gen script

--- scripts/chem_data.sh
+++ scripts/chem_data.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-source activate gscan
 
 num_obj=$1
 num_colors=$2
-python data_gen/env.py --env_id ColorChanging-$num_obj-$num_colors-v0 --fname data/ColorChanging_$num_obj-$num_colors-train.h5  --num_episodes 1000 --episode-length 100 --seed 1
-python data_gen/env.py --env_id ColorChanging-$num_obj-$num_colors-v0 --fname data/ColorChanging_$num_obj-$num_colors-valid.h5  --num_episodes 200 --episode-length 100 --seed 3
-python data_gen/env.py --env_id ColorChanging-$num_obj-$num_colors-v0 --fname data/ColorChanging_$num_obj-$num_colors-test.h5  --num_episodes 10000 --episode-length 10 --seed 2
-
-
+seed=$3
+graph=$4
+max_steps=$5
+movement=$6
+python ../data_gen/env.py --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --fname data/ColorChangingRL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5  --num-episodes 1000 --episode-length 100 --seed $seed --save_graph --save_graph_location data/ColorChangingRL_$num_obj-$num_colors-$max_steps-$movement-train-graph-$seed --graph $graph
+python ../data_gen/env.py --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --fname data/ColorChangingRL_$num_obj-$num_colors-$max_steps-$movement-valid-$seed.h5  --num-episodes 200 --episode-length 100 --seed 3 --load_graph --load_graph_location data/ColorChangingRL_$num_obj-$num_colors-$max_steps-$movement-train-graph-$seed
+python ../data_gen/env.py --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --fname data/ColorChangingRL_$num_obj-$num_colors-$max_steps-$movement-test-$seed.h5  --num-episodes 10000 --episode-length 10 --seed 2 --load_graph --load_graph_location data/ColorChangingRL_$num_obj-$num_colors-$max_steps-$movement-train-graph-$seed
 
