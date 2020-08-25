87d02a4b77f1b00becfd2c51fffc5ae974d5ad22
dido1998 <adidolkar123@gmail.com>
2020-08-25 16:27:21 +0530

    reward prediction script for chem

--- scripts/chem_reward.sh
+++ scripts/chem_reward.sh
@@ -27,38 +27,39 @@ touch "$save_folder-2/train_rl.log"
 touch "$save_folder-3/train_rl.log"
 
 save_folder_="$save_folder-1"
+model_folder="${model}_${num_obj}--${num_colors}-${seed}"
+python train_reward_predictor.py --save-folder "$save_folder_/$model_folder" --random
+python train_reward_predictor.py --save-folder "$save_folder_/$model_folder" --finetune
+python test_planning.py --save-folder "$save_folder_/$model_folder" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 1
+python test_planning.py --save-folder "$save_folder_/$model_folder" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 5
+python test_planning.py --save-folder "$save_folder_/$model_folder" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 10
 
-python train_reward_predictor.py --save-folder "$save_folder_" --random
-python train_reward_predictor.py --save-folder "$save_folder_" --finetune
-python test_planning.py --save-folder "$save_folder_" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 1
-python test_planning.py --save-folder "$save_folder_" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 5
-python test_planning.py --save-folder "$save_folder_" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 10
-
-python test_planning.py --save-folder "$save_folder_" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 1
-python test_planning.py --save-folder "$save_folder_" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 5
-python test_planning.py --save-folder "$save_folder_" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 10
+python test_planning.py --save-folder "$save_folder_/$model_folder" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 1
+python test_planning.py --save-folder "$save_folder_/$model_folder" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 5
+python test_planning.py --save-folder "$save_folder_/$model_folder" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 10
 
 
 save_folder_="$save_folder-2"
+model_folder="${model}_${num_obj}--${num_colors}-${seed}"
+python train_reward_predictor.py --save-folder "$save_folder_/$model_folder" --random
+python train_reward_predictor.py --save-folder "$save_folder_/$model_folder" --finetune
+python test_planning.py --save-folder "$save_folder_/$model_folder" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 1
+python test_planning.py --save-folder "$save_folder_/$model_folder" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 5
+python test_planning.py --save-folder "$save_folder_/$model_folder" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 10
 
-python train_reward_predictor.py --save_folder "$save_folder_" --random
-python train_reward_predictor.py --save_folder "$save_folder_" --finetune
-python test_planning.py --save_folder "$save_folder_" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 1
-python test_planning.py --save_folder "$save_folder_" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 5
-python test_planning.py --save_folder "$save_folder_" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 10
-
-python test_planning.py --save_folder "$save_folder_" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 1
-python test_planning.py --save_folder "$save_folder_" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 5
-python test_planning.py --save_folder "$save_folder_" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 10
+python test_planning.py --save-folder "$save_folder_/$model_folder" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 1
+python test_planning.py --save-folder "$save_folder_/$model_folder" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 5
+python test_planning.py --save-folder "$save_folder_/$model_folder" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 10
 
 save_folder_="$save_folder-3"
 
-python train_reward_predictor.py --save_folder "$save_folder_" --random
-python train_reward_predictor.py --save_folder "$save_folder_" --finetune
-python test_planning.py --save_folder "$save_folder_" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 1
-python test_planning.py --save_folder "$save_folder_" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 5
-python test_planning.py --save_folder "$save_folder_" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 10
+model_folder="${model}_${num_obj}--${num_colors}-${seed}"
+python train_reward_predictor.py --save-folder "$save_folder_/$model_folder" --random
+python train_reward_predictor.py --save-folder "$save_folder_/$model_folder" --finetune
+python test_planning.py --save-folder "$save_folder_/$model_folder" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 1
+python test_planning.py --save-folder "$save_folder_/$model_folder" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 5
+python test_planning.py --save-folder "$save_folder_/$model_folder" --random --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 10
 
-python test_planning.py --save_folder "$save_folder_" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 1
-python test_planning.py --save_folder "$save_folder_" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 5
-python test_planning.py --save_folder "$save_folder_" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_" --num-steps 10
+python test_planning.py --save-folder "$save_folder_/$model_folder" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 1
+python test_planning.py --save-folder "$save_folder_/$model_folder" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 5
+python test_planning.py --save-folder "$save_folder_/$model_folder" --finetune --env-id ColorChangingRL-$num_obj-$num_colors-$movement-$max_steps-v0 --save "$save_folder_/$model_folder" --num-steps 10
