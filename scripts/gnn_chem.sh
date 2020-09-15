d460ccb9013c6ddcc11817a2f7274bdebd3226ab
dido1998 <adidolkar123@gmail.com>
2020-09-16 05:10:48 +0530

    updated scripts

--- scripts/gnn_chem.sh
+++ scripts/gnn_chem.sh
@@ -10,9 +10,15 @@ time=$7
 edge=$8
 if [ -z "$8" ]
 then
-	save_folder=gnn-$num_obj-$num_colors-$max_steps-$movement-$seed-$contrastive_loss-$time
+	save_folder=ae-$num_obj-$num_colors-$max_steps-$movement-$seed-$contrastive_loss-$time
+	dataset1=data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5
+	dataset2=data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-test-$seed.h5
+	dataset3=data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-valid-$seed.h5
 else
-	save_folder=gnn-$num_obj-$num_colors-$edge-$max_steps-$movement-$seed-$contrastive_loss-$time
+	save_folder=ae-$num_obj-$num_colors-$edge-$max_steps-$movement-$seed-$contrastive_loss-$time
+	dataset1=data/ColorChanging${time}RL_$num_obj-$num_colors-$edge-$max_steps-$movement-train-$seed.h5
+	dataset2=data/ColorChanging${time}RL_$num_obj-$num_colors-$edge-$max_steps-$movement-test-$seed.h5
+	dataset3=data/ColorChanging${time}RL_$num_obj-$num_colors-$edge-$max_steps-$movement-valid-$seed.h5
 fi
 truth=True
 
@@ -31,56 +37,56 @@ touch "$save_folder-3/train.log"
 if [ $contrastive_loss == $truth ]
 then
 	save_folder_="$save_folder-1"
-  	python train_baselines.py --dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5 \
+  	python train_baselines.py --dataset $dataset1 \
 	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-test-$seed.h5 \
+	--eval-dataset $dataset2 \
 	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-valid-$seed.h5 \
+	--valid-dataset $dataset3 \
 	--save-folder $save_folder_ --batch-size 512 --seed 1 \
 	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn --contrastive | tee -a "$save_folder_/train.log"
 
 	save_folder_="$save_folder-2"
-  	python train_baselines.py --dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5 \
+  	python train_baselines.py --dataset $dataset1 \
 	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-test-$seed.h5 \
+	--eval-dataset $dataset2 \
 	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-valid-$seed.h5 \
+	--valid-dataset $dataset3 \
 	--save-folder $save_folder_ --batch-size 512 --seed 2 \
 	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn --contrastive | tee -a "$save_folder_/train.log"
 
 	save_folder_="$save_folder-3"
-  	python train_baselines.py --dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5 \
+  	python train_baselines.py --dataset $dataset1 \
 	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-test-$seed.h5 \
+	--eval-dataset $dataset2 \
 	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-valid-$seed.h5 \
+	--valid-dataset $dataset3 \
 	--save-folder $save_folder_ --batch-size 512 --seed 3 \
 	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn --contrastive | tee -a "$save_folder_/train.log"
 else
     save_folder_="$save_folder-1"
-  	python train_baselines.py --dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5 \
+  	python train_baselines.py --dataset $dataset1 \
 	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-test-$seed.h5 \
+	--eval-dataset $dataset2 \
 	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-valid-$seed.h5 \
+	--valid-dataset $dataset3 \
 	--save-folder $save_folder_ --batch-size 512 --seed 1 \
 	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn | tee -a "$save_folder_/train.log"
 
 	save_folder_="$save_folder-2"
-  	python train_baselines.py --dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5 \
+  	python train_baselines.py --dataset $dataset1 \
 	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-test-$seed.h5 \
+	--eval-dataset $dataset2 \
 	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-valid-$seed.h5 \
+	--valid-dataset $dataset3 \
 	--save-folder $save_folder_ --batch-size 512 --seed 2 \
 	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn | tee -a "$save_folder_/train.log"
 
 	save_folder_="$save_folder-3"
-  	python train_baselines.py --dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5 \
+  	python train_baselines.py --dataset $dataset1 \
 	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-test-$seed.h5 \
+	--eval-dataset $dataset2 \
 	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-valid-$seed.h5 \
+	--valid-dataset $dataset3 \
 	--save-folder $save_folder_ --batch-size 512 --seed 3 \
 	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn  | tee -a "$save_folder_/train.log"
 fi
