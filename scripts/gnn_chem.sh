ba4cdea4d23bef5f18eb4e0819069faf6c57fdb8
dido1998 <adidolkar123@gmail.com>
2020-09-20 16:53:31 +0530

    updates

--- scripts/gnn_chem.sh
+++ scripts/gnn_chem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ../modular_central/torch/bin/activate
+source ../torch/bin/activate
 
 num_obj=$1
 num_colors=$2
@@ -8,9 +8,10 @@ seed=$3
 contrastive_loss=$4
 max_steps=$5
 movement=$6
-time=$7
-edge=$8
-if [ -z "$8" ]
+idx=$7
+time=$8
+edge=$9
+if [ -z "$9" ]
 then
 	save_folder=gnn-$num_obj-$num_colors-$max_steps-$movement-$seed-$contrastive_loss-$time
 	dataset1=data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5
@@ -24,21 +25,15 @@ else
 fi
 truth=True
 
-rm -r "$save_folder-1"
-rm -r "$save_folder-2"
-rm -r "$save_folder-3"
+rm -r "$save_folder-$idx"
 
-mkdir "$save_folder-1"
-mkdir "$save_folder-2"
-mkdir "$save_folder-3"
+mkdir "$save_folder-$idx"
 
-touch "$save_folder-1/train.log"
-touch "$save_folder-2/train.log"
-touch "$save_folder-3/train.log"
+touch "$save_folder-$idx/train.log"
 
 if [ $contrastive_loss == $truth ]
 then
-	save_folder_="$save_folder-1"
+	save_folder_="$save_folder-$idx"
   	python train_baselines.py --dataset $dataset1 \
 	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
 	--eval-dataset $dataset2 \
@@ -47,25 +42,9 @@ then
 	--save-folder $save_folder_ --batch-size 512 --seed 1 \
 	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn --contrastive | tee -a "$save_folder_/train.log"
 
-	save_folder_="$save_folder-2"
-  	python train_baselines.py --dataset $dataset1 \
-	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 512 --seed 2 \
-	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn --contrastive | tee -a "$save_folder_/train.log"
-
-	save_folder_="$save_folder-3"
-  	python train_baselines.py --dataset $dataset1 \
-	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 512 --seed 3 \
-	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn --contrastive | tee -a "$save_folder_/train.log"
+	
 else
-    save_folder_="$save_folder-1"
+    save_folder_="$save_folder-$idx"
   	python train_baselines.py --dataset $dataset1 \
 	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
 	--eval-dataset $dataset2 \
@@ -73,23 +52,5 @@ else
 	--valid-dataset $dataset3 \
 	--save-folder $save_folder_ --batch-size 512 --seed 1 \
 	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn | tee -a "$save_folder_/train.log"
-
-	save_folder_="$save_folder-2"
-  	python train_baselines.py --dataset $dataset1 \
-	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 512 --seed 2 \
-	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn | tee -a "$save_folder_/train.log"
-
-	save_folder_="$save_folder-3"
-  	python train_baselines.py --dataset $dataset1 \
-	--encoder "medium" --name  "gnn_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 512 --seed 3 \
-	--epochs 100 --pretrain-epochs 100 --predict-diff --gnn  | tee -a "$save_folder_/train.log"
 fi
 
--- scripts/modular_chem.sh
+++ scripts/modular_chem.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+source ../torch/bin/activate
+
 num_obj=$1
 num_colors=$2
 seed=$3
 contrastive_loss=$4
 max_steps=$5
 movement=$6
-time=$7
-edge=$8
-if [ -z "$8" ]
+idx=$7
+time=$8
+edge=$9
+if [ -z "$9" ]
 then
 	save_folder=modular-$num_obj-$num_colors-$max_steps-$movement-$seed-$contrastive_loss-$time
 	dataset1=data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5
@@ -22,48 +25,26 @@ else
 fi
 truth=True
 
-rm -r "$save_folder-1"
-rm -r "$save_folder-2"
-rm -r "$save_folder-3"
+rm -r "$save_folder-$idx"
 
-mkdir "$save_folder-1"
-mkdir "$save_folder-2"
-mkdir "$save_folder-3"
+mkdir "$save_folder-$idx"
 
-touch "$save_folder-1/train.log"
-touch "$save_folder-2/train.log"
-touch "$save_folder-3/train.log"
+
+touch "$save_folder-$idx/train.log"
+
 
 if [ $contrastive_loss == $truth ]
 then
-	save_folder_="$save_folder-1"
+	save_folder_="$save_folder-$idx"
   	python train_baselines.py --dataset $dataset1 \
 	--encoder "medium" --name  "modular_$num_obj-$size-$num_colors-$seed"   \
 	--eval-dataset $dataset2 \
 	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
 	--valid-dataset $dataset3 \
 	--save-folder $save_folder_ --batch-size 512 --seed 1 \
-	--epochs 100 --pretrain-epochs 100 --predict-diff --modular --contrastive | tee -a "$save_folder_/train.log"
-
-	save_folder_="$save_folder-2"
-  	python train_baselines.py --dataset $dataset1 \
-	--encoder "medium" --name  "modular_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 512 --seed 2 \
-	--epochs 100 --pretrain-epochs 100 --predict-diff --modular --contrastive | tee -a "$save_folder_/train.log"
-
-	save_folder_="$save_folder-3"
-  	python train_baselines.py --dataset $dataset1 \
-	--encoder "medium" --name  "modular_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 512 --seed 3 \
 	--epochs 100 --pretrain-epochs 100 --predict-diff --modular --contrastive | tee -a "$save_folder_/train.log"
 else
-    save_folder_="$save_folder-1"
+    save_folder_="$save_folder-$idx"
   	python train_baselines.py --dataset $dataset1 \
 	--encoder "medium" --name  "modular_$num_obj-$size-$num_colors-$seed"   \
 	--eval-dataset $dataset2 \
@@ -72,22 +53,6 @@ else
 	--save-folder $save_folder_ --batch-size 512 --seed 1 \
 	--epochs 100 --pretrain-epochs 100 --predict-diff --modular | tee -a "$save_folder_/train.log"
 
-	save_folder_="$save_folder-2"
-  	python train_baselines.py --dataset $dataset1 \
-	--encoder "medium" --name  "modular_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 512 --seed 2 \
-	--epochs 100 --pretrain-epochs 100 --predict-diff --modular | tee -a "$save_folder_/train.log"
-
-	save_folder_="$save_folder-3"
-  	python train_baselines.py --dataset $dataset1 \
-	--encoder "medium" --name  "modular_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 512 --seed 3 \
-	--epochs 100 --pretrain-epochs 100 --predict-diff --modular  | tee -a "$save_folder_/train.log"
+	
 fi
 
