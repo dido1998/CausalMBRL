79e8ee4c301534995c5aa74e91aa5184421ec954
dido1998 <adidolkar123@gmail.com>
2020-09-22 22:22:29 +0530

    updates

--- scripts/lstm_chem.sh
+++ scripts/lstm_chem.sh
@@ -6,9 +6,10 @@ seed=$3
 contrastive_loss=$4
 max_steps=$5
 movement=$6
-time=$7
-edge=$8
-if [ -z "$8" ]
+idx=$7
+time=$8
+edge=$9
+if [ -z "$9" ]
 then
 	save_folder=lstm-$num_obj-$num_colors-$max_steps-$movement-$seed-$contrastive_loss-$time
 	dataset1=data/ColorChanging${time}RL_$num_obj-$num_colors-$max_steps-$movement-train-$seed.h5
@@ -22,75 +23,36 @@ else
 fi
 truth=True
 
-rm -r "$save_folder-1"
-rm -r "$save_folder-2"
-rm -r "$save_folder-3"
+rm -r "$save_folder-$idx"
 
-mkdir "$save_folder-1"
-mkdir "$save_folder-2"
-mkdir "$save_folder-3"
 
-touch "$save_folder-1/train.log"
-touch "$save_folder-2/train.log"
-touch "$save_folder-3/train.log"
+mkdir "$save_folder-$idx"
+
+touch "$save_folder-$idx/train.log"
+
 
 
 if [ $contrastive_loss == $truth ]
 then
-	save_folder_="$save_folder-1"
+	save_folder_="$save_folder-$idx"
   	python train_recurrent.py --dataset $dataset1 \
 	--encoder "medium" --name  "lstm_$num_obj-$size-$num_colors-$seed"   \
 	--eval-dataset $dataset2 \
 	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
 	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 32 --seed 1 \
-	--epochs 0 --finetune-epochs 100 --predict-diff  --contrastive | tee -a "$save_folder_/train.log"
-
-    save_folder_="$save_folder-2"
-  	python train_recurrent.py --dataset $dataset1 \
-	--encoder "medium" --name  "lstm_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 32 --seed 2 \
-	--epochs 0 --finetune-epochs 100 --predict-diff  --contrastive | tee -a "$save_folder_/train.log"
-
-	save_folder_="$save_folder-3"
-  	python train_recurrent.py --dataset $dataset1 \
-	--encoder "medium" --name  "lstm_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 32 --seed 3 \
-	--epochs 0 --finetune-epochs 100 --predict-diff  --contrastive | tee -a "$save_folder_/train.log"
+	--save-folder $save_folder_ --batch-size 32 --seed $idx \
+	--epochs 100 --pretrain-epochs 100 --finetune-epochs 100 --predict-diff  --contrastive | tee -a "$save_folder_/train.log"
 
 else
-    save_folder_="$save_folder-1"
+    save_folder_="$save_folder-$idx"
   	python train_recurrent.py --dataset $dataset1 \
 	--encoder "medium" --name  "lstm_$num_obj-$size-$num_colors-$seed"   \
 	--eval-dataset $dataset2 \
 	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
 	--valid-dataset $dataset3 \
 	--save-folder $save_folder_ --batch-size 32 --seed 1 \
-	--epochs 0 --finetune-epochs 100 --predict-diff  | tee -a "$save_folder_/train.log"
+	--epochs 100 --pretrain-epochs 100 --finetune-epochs 100 --predict-diff  | tee -a "$save_folder_/train.log"
 
-    save_folder_="$save_folder-2"
-  	python train_recurrent.py --dataset $dataset1 \
-	--encoder "medium" --name  "lstm_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 32 --seed 2 \
-	--epochs 0 --finetune-epochs 100 --predict-diff  | tee -a "$save_folder_/train.log"
-
-	save_folder_="$save_folder-3"
-  	python train_recurrent.py --dataset $dataset1 \
-	--encoder "medium" --name  "lstm_$num_obj-$size-$num_colors-$seed"   \
-	--eval-dataset $dataset2 \
-	--embedding-dim-per-object 32 --num-objects $num_obj --action-dim $num_colors  \
-	--valid-dataset $dataset3 \
-	--save-folder $save_folder_ --batch-size 32 --seed 3 \
-	--epochs 0 --finetune-epochs 100 --predict-diff  | tee -a "$save_folder_/train.log"
-
+    
 fi
 
